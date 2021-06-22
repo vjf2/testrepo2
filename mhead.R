59ff3c60116d19cab6a151f3d1f1a12a8bc2cacd
vjf2 <foroughirad@gmail.com>
2021-06-22 12:30:34 -0400

    fixed spacing

--- mhead.R
+++ mhead.R
@@ -1,6 +1,6 @@
 #' A Matrix Preview Function
 #'
-#' This function allows you to view a preview of wide matrices and dataframes.
+#' This mhead (matrix head) function allows you to view a preview of wide matrices and dataframes.
 #' @param x a matrix or dataframe
 #' @param n number of row or columns to display (default 10)
 #' @keywords matrix head
@@ -9,11 +9,11 @@
 #' mhead()
 
 
-mhead<-function(x, n=10){
-  nr<-nrow(x)
-  nc<-ncol(x)
-  ifelse(nr<=n, nr<-nr, nr<-n)
-  ifelse(nc<=n, nc<-nc, nc<-n)
+mhead <- function(x, n = 10){
+  nr <- nrow(x)
+  nc <- ncol(x)
+  ifelse(nr <= n, nr <- nr, nr <- n)
+  ifelse(nc <= n, nc <- nc, nc <- n)
   return(x[1:nr, 1:nc])
 
 
